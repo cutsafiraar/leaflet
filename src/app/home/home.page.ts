07fe634994ce4a492dc43def631f4508b701fba9
cutsafiraar <cut.aulia.safira.rachman@mail.ugm.ac.id>
2023-09-25 10:42:36 +0700

    Base Project ionic-leaflet

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  map!: L.Map;
+
+  constructor() { }
+  //ngOnInit() {
+
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([-7.790066200452867, 110.36421392003751], 11)
+
+    var OpenStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    var EsriWorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
+
+    var EsriNatGeo = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
+      maxZoom: 16
+    });
+
+    var CartoDark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 20
+    });
+
+    // Membuat objek untuk kontrol lapisan
+    var baseMap = {
+      "Open Street Map": OpenStreetMap,
+      "Esri World Imagery": EsriWorldImagery,
+      "Esri Nat Geo": EsriNatGeo,
+      "Carto Dark": CartoDark
+    };
+
+    // Tambahkan kontrol lapisan ke peta
+    L.control.layers(baseMap).addTo(this.map);
+
+    L.marker([-7.767669524047528, 110.37775455941811]).addTo(this.map)
+      .bindPopup('halo, disini UGM.')
+      .openPopup();
+  }
+}
